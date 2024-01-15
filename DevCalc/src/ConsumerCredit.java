b95abd6898c7aec4a58a963df8db09805e8a1d66
1 <1@LAPTOP-5030DCQR>
2024-01-15 22:23:24 +0500

    Rjvvb

--- DevCalc/src/ConsumerCredit.java
+++ DevCalc/src/ConsumerCredit.java
@@ -5,7 +5,7 @@ public class ConsumerCredit extends Credit {
 
 	@Override
 	public void numberOfPayments(Double loanAmount) {
-		Integer numberOfPayments =getLoanTermInYears() * 12;
+//		Integer numberOfPayments =getLoanTermInYears() * 12;
 		
 	}
 
